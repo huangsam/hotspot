4f09d3b2d9cf33982fe2de21ed9b6812f7b097ca
Samuel Huang <samhuang91@gmail.com>
2025-11-02 19:51:35 -0800

    Add tests for plain labeling

---
+++ internal/internal_test.go
@@ -0,0 +1,56 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPlainLabel(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    float64
+		expected string
+	}{
+		{
+			name:     "smallest value possible",
+			input:    0.0,
+			expected: lowValue,
+		},
+		{
+			name:     "just before moderate",
+			input:    39.9,
+			expected: lowValue,
+		},
+		{
+			name:     "exactly moderate",
+			input:    40.0,
+			expected: moderateValue,
+		},
+		{
+			name:     "just before high",
+			input:    59.9,
+			expected: moderateValue,
+		},
+		{
+			name:     "exactly high",
+			input:    60.0,
+			expected: highValue,
+		},
+		{
+			name:     "just before critical",
+			input:    79.9,
+			expected: highValue,
+		},
+		{
+			name:     "exactly critical",
+			input:    80.0,
+			expected: criticalValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, getPlainLabel(tt.input))
+		})
+	}
+}
